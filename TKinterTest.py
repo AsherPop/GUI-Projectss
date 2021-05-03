869189f449c51362bd3e7f7ee3168feeefae3eb4
AsherPop <77747352+AsherPop@users.noreply.github.com>
2021-05-03 10:46:15 -0700

    Add files via upload

---
+++ TKinterTest.py
@@ -0,0 +1,22 @@
+from tkinter import *
+
+top = Tk()
+
+
+L1 = Label(top, text="Hello, World!")
+
+L1.grid(column = 0, row = 1)
+
+E1 = Entry(top, bd = 5)
+
+E1.grid(column = 1, row = 2)
+
+
+
+
+
+
+
+top.mainLoop()
+
+
